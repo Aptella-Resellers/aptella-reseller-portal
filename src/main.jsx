6541074f63bf6fe8cdd4c3adc41c35ae3f26fc6d
taraherley <tara.k.herley@gmail.com>
2025-08-20 10:32:02 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DealRegistrationPortal from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DealRegistrationPortal />
+  </React.StrictMode>,
+)
