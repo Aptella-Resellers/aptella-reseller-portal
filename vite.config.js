125f604338fc9db319585433c975b77bcecea2fd
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-25 12:01:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: set your GitHub Pages base to the repo name:
-const base = '/aptella-reseller-portal/'
-
 export default defineConfig({
-  base,
+  base: '/aptella-reseller-portal/',   // keep this for GH Pages
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: false,
-  },
+  optimizeDeps: {
+    include: ['leaflet', 'leaflet.markercluster']
+  }
 })
