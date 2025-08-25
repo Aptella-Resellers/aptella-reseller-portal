074c21019cdee54dff3b4df58a0073220b9b5598
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-25 12:27:21 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import './index.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DealRegistrationPortal from './App.jsx'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DealRegistrationPortal />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
