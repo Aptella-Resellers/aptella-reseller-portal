1f7ee6cbc3fb3daad079bc72208c0d12d2c899a3
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-26 12:37:13 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         "aptella-navy": "#0e3446",
-        "aptella-orange": "#f0a03a",
+        "aptella-navy-dark": "#0b2938",
+        "aptella-orange": "#f0a03a"
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 };
