9267ccdaefc8786286663f11e112167d1d14c9e6
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-26 12:42:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // IMPORTANT for GitHub Pages project sites:
-  base: "/aptella-reseller-portal/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
 });
