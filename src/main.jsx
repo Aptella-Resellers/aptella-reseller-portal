a0eca5e5071405323f9918cf8677e53baf232798
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-28 07:04:37 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import AptellaRoot from "./App.jsx";
+import "./index.css";
 
-// Leaflet CSS (needed once globally)
-import 'leaflet/dist/leaflet.css';
-
-import App from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <AptellaRoot />
   </React.StrictMode>
 );
