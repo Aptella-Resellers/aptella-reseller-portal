d2b6a7dce0e1300712ae2b78c64f0831dad41e85
Mathew Connelly <tara.k.herley@gmail.com>
2025-08-27 12:50:02 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
+// Leaflet CSS (needed once globally)
+import 'leaflet/dist/leaflet.css';
+
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
